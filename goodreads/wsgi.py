b4eb79ede14c3d385f117a6e42915dfff16b1613
Leonardo Ramírez <leonardon473@gmail.com>
2017-10-04 20:56:43 -0500

    Cambio a wsgi.py

--- goodreads/wsgi.py
+++ goodreads/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 import dotenv
-import django.core.wsgi
+from django.core.wsgi import get_wsgi_application
 try:
     dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
     load_dotenv(dotenv_path)
